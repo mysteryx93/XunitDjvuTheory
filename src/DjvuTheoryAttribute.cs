cb50c5777d615c22be0a03d9c304e32b3864bb0f
Etienne Charland <etienne@emergenceguardian.com>
2019-02-16 21:11:28 +0700

    Upload code

---
+++ src/DjvuTheoryAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace DjvuNet.Tests.Xunit
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer("DjvuNet.Tests.Xunit.DjvuTheoryDiscoverer", global::AssemblyData.Name)]
+    public class DjvuTheoryAttribute : FactAttribute
+    {
+    }
+}
